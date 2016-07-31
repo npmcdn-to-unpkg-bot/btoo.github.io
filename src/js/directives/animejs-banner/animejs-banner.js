b11cb55963581d2e5b415bf54ff3a1d5b530a295
Brian <brian@trackmind.com>
2016-07-31 02:43:15 -0400

    testing loading screen

--- src/js/directives/animejs-banner/animejs-banner.js
+++ src/js/directives/animejs-banner/animejs-banner.js
@@ -5,74 +5,98 @@ portfolio.directive("animejsBanner", [ /*"ColorService",*/
 			templateUrl: 'js/directives/animejs-banner/animejs-banner.html',
 			link: function(scope, element, attr, ctrl) {
 
-				var squares = new Promise((resolve, reject)=>{
 
-					var columns = Math.ceil(element.width()/88);
-					var rows = Math.ceil(element.height()/88);
-					var cells = columns*rows;
+				// IE cannot apply CSS transforms on SVG elements. (See https://connect.microsoft.com/IE/feedback/details/811744/ie11-bug-with-implementation-of-css-transforms-in-svg)
+				var path = anime.path('#Bpath');
 
-					var x, y, xmax, ymin, xincrement, yincrement;
-					x = y = xmax = ymin = 0;
-					xincrement = yincrement = 88;
-
-					do {
-						element[0].childNodes[0].innerHTML +=	`
-																	<article style="width:88px;height:88px;top:`+y+`px;right:`+x+`px">
-																		<div class="blue"></div>
-																	</article>
-																`;
-
-						if (x < xincrement || y > element.height()-yincrement) {
-							if (xmax < element.width()) {
-								xmax += xincrement;
-							} else {
-								ymin += yincrement;
-							}
-							y = ymin;
-							x = xmax;
-						} else {
-							x -= xincrement;
-							y += yincrement;
-						}
-
-					} while (x < element.width() || y < element.height())
-
-					resolve();
-
+				anime({
+					targets: '#loading-squares div',
+					translateX: path,
+					translateY: path,
+					rotate: path,
+					duration: 10000,
+					loop: true,
+					easing: 'linear'
 				});
 
-				squares.then(()=>{
-					console.log('finished building squares, so now animate');
-					anime({
-						targets: '#squares div',
-						rotate: {
-							value: 180,
-							duration: 1500,
-							easing: 'easeInOutQuad'
-						},
-						scale: {
-							value: [0, 1],
-							// delay: 150,
-							duration: 1500,
-							easing: 'easeInOutExpo',
-						},
-						loop: false,
-						complete: () => {
-							element[0].childNodes[0].className += 'blue';
-							element[0].childNodes[0].innerHTML = ''; //clear the squares so they dont take up memory
-						},
-						direction: 'normal',
-						borderRadius: {
-							value: 0,
-							duration: 2000,
-							easing: 'easeInOutExpo'
-						},
-						delay: (el, index) => {
-							return index * 22;
-						}
+				// anime({
+				// 	targets: 'path',
+				// 	opacity: 0,
+				// 	duration: 6000,
+				// 	loop: true,
+				// 	direction: 'alternate',
+				// 	easing: 'easeInOutExpo'
+				// });
 
-					});
-				});
+
+
+				// var squares = element[0].querySelector('#squares');
+
+				// var createSquares = new Promise((resolve, reject)=>{
+
+				// 	var x, y, xmax, ymin, xincrement, yincrement;
+				// 	x = y = xmax = ymin = 0;
+				// 	xincrement = yincrement = 88; //width and height of each square
+
+				// 	do {
+				// 		squares.innerHTML +=	`
+				// 									<article style="width:`+xincrement+`px;height:`+yincrement+`px;top:`+y+`px;right:`+x+`px">
+				// 										<div class="blue"></div>
+				// 									</article>
+				// 								`;
+
+				// 		if (x < xincrement || y > element.height()-yincrement) {
+				// 			if (xmax < element.width()) {
+				// 				xmax += xincrement;
+				// 			} else {
+				// 				ymin += yincrement;
+				// 			}
+				// 			y = ymin;
+				// 			x = xmax;
+				// 		} else {
+				// 			x -= xincrement;
+				// 			y += yincrement;
+				// 		}
+
+				// 	} while (x < element.width() || y < element.height())
+
+				// 	resolve();
+				// });
+
+				// createSquares.then(()=>{
+				// 	console.log('finished building squares, so now animate');
+				// 	anime({
+				// 		targets: '#squares div',
+				// 		rotate: {
+				// 			value: 180,
+				// 			duration: 1500,
+				// 			easing: 'easeInOutQuad'
+				// 		},
+				// 		scale: {
+				// 			value: [0, 1],
+				// 			// delay: 150,
+				// 			duration: 1500,
+				// 			easing: 'easeInOutExpo'
+				// 		},
+				// 		loop: false,
+				// 		complete: () => {
+				// 			squares.className += 'blue';
+				// 			squares.innerHTML = ''; //clear the squares so they dont take up memory
+				// 		},
+				// 		direction: 'normal',
+				// 		borderRadius: {
+				// 			value: 0,
+				// 			duration: 2000,
+				// 			easing: 'easeInOutExpo'
+				// 		},
+				// 		delay: (el, index, total) => {
+				// 			// console.log(el, index, total);
+				// 			// return index * 22;
+				// 			return index * (2500 / total); //the whole animation takes 2 seconds total
+				// 		},
+
+				// 	});
+				// });
 
 			}
 		};
