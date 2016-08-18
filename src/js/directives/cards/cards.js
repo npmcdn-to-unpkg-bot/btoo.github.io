bf9384cb87d11e57855aa944e741c2a2001e3deb
Brian <brian@trackmind.com>
2016-08-17 23:12:31 -0400

    testing new cards carousel

--- src/js/directives/cards/cards.js
+++ src/js/directives/cards/cards.js
@@ -1,28 +1,3 @@
-portfolio.factory('carouselService', [
-	function(){
-		var itemCount = 0;
-		var activeItem = null;
-
-		return {
-			itemCount: () => { return itemCount; },
-			activeItem: () => { return activeItem; },
-			addItem: function() {
-				var newId = itemCount++;
-				activeItem = itemCount === 1 ? newId : activeItem;
-				return newId;
-			},
-			next: function() {
-				activeItem = activeItem || 0;
-				activeItem = activeItem === itemCount - 1 ? 0 : activeItem + 1;
-			},
-			prev: function() {
-				activeItem = activeItem || 0;
-				activeItem = activeItem === 0 ? itemCount - 1 : activeItem - 1;
-			}
-		}
-	}
-]);
-
 portfolio.directive('carouselItem', ['$drag', 'carouselService',
 	function($drag, carouselService) {
 		return {
