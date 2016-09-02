6137fcc817d058b1562300763f8ed6fba3bc6408
Brian <feihcsim824@gmail.com>
2016-09-01 21:03:49 -0400

    filtering styling + animations

---
+++ src/js/directives/skillz/skill.js
@@ -0,0 +1,12 @@
+portfolio.directive('skill', [
+	'$animate',
+	function($animate){
+		return {
+			link: (scope, element, attrs) => {
+				console.log(scope);
+				// console.log(skillzService);
+				// console.log($rootScope);
+			}
+		}
+	}
+]);
